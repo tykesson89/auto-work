365081652896710c7031e5dd5bf7cf3ef549e547
tykesson89 <tykesson89@gmail.com>
2016-03-11 12:57:13 +0100

    En första version av designen

--- app/src/main/java/com/lhadalo/oladahl/autowork/SplashScreenActivity.java
+++ app/src/main/java/com/lhadalo/oladahl/autowork/SplashScreenActivity.java
@@ -3,11 +3,11 @@ package com.lhadalo.oladahl.autowork;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
