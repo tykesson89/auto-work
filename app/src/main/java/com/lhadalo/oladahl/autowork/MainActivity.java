dd4f5d52ec79b9714552c5d78573942b645f4e9d
Ola Dahl <oladahl.lel@gmail.com>
2016-03-26 10:01:29 +0100

    refactor: Flytta logga ut och inställningar till meny

--- app/src/main/java/com/lhadalo/oladahl/autowork/MainActivity.java
+++ app/src/main/java/com/lhadalo/oladahl/autowork/MainActivity.java
@@ -3,13 +3,18 @@ package com.lhadalo.oladahl.autowork;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-        private Button btnSettings, btnLogOut;
-        private TextView tvName;
+    private Toolbar toolbar;
+    private Button btnSettings, btnLogOut;
+    private TextView tvName;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -21,30 +26,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void initComponents(){
-        btnSettings = (Button)findViewById(R.id.btnSettings);
-        btnLogOut = (Button)findViewById(R.id.btnLogOut);
-        tvName = (TextView)findViewById(R.id.tvName);
+    public void initComponents() {
+        btnSettings = (Button) findViewById(R.id.btnSettings);
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        tvName = (TextView) findViewById(R.id.tvName);
     }
 
-    public void initListeners(){
+    public void initListeners() {
         btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(intent);
-                finish();
+
             }
         });
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               SQLiteDB sqLiteDB = new SQLiteDB(MainActivity.this);
-                sqLiteDB.deleteAll();
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Intent intent;
+        switch (id) {
+            case R.id.action_settings:
+                intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.action_log_out:
+                SQLiteDB sqLiteDB = new SQLiteDB(MainActivity.this);
+                sqLiteDB.deleteAll();
+                intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
