141db69655f79cf2c3c122074f2c16b870ef6eff
Ola Dahl <oladahl.lel@gmail.com>
2016-03-28 09:29:56 +0200

    feat: Lägg till lyssnare till listelement lägga i till arbetspass

--- app/src/main/java/com/lhadalo/oladahl/autowork/AddWorkpassActivity.java
+++ app/src/main/java/com/lhadalo/oladahl/autowork/AddWorkpassActivity.java
@@ -5,50 +5,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by oladahl on 16-03-26.
  */
-public class AddWorkpassActivity extends AppCompatActivity {
+public class AddWorkpassActivity extends AppCompatActivity implements AddWorkpassFragment.OnFragmentInteraction{
+    private AddWorkpassFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_add_workpass);
+        setContentView(R.layout.activity_add_workpass);
 
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_1);
-        setSupportActionBar(toolbar);
-        
-        setTitle("Add workpass");
+        initFragment();
+    }
 
+    private void initFragment(){
+        fragment = new AddWorkpassFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container_add_workpass, fragment).commit();
+    }
 
-        LinearLayout layoutWorkplace = (LinearLayout)findViewById(R.id.txt_layout_workplace);
+    @Override
+    public void onClickWorkplace() {
+        Toast.makeText(this, "Workplace", Toast.LENGTH_SHORT).show();
+    }
 
-        LinearLayout layoutStart = (LinearLayout)findViewById(R.id.txt_layout_start);
-        LinearLayout layoutStop = (LinearLayout)findViewById(R.id.txt_layout_stop);
-        LinearLayout layoutLunch = (LinearLayout)findViewById(R.id.txt_layout_lunch);
-        LinearLayout layoutAddNote = (LinearLayout)findViewById(R.id.txt_layout_add_notes);
-
-        TextView text1 = (TextView)layoutWorkplace.getChildAt(1);
-        TextView textDate1 = (TextView)layoutStart.getChildAt(1);
-        TextView textTime1 = (TextView)layoutStart.getChildAt(2);
-
-        TextView textDate2 = (TextView)layoutStop.getChildAt(1);
-
-        TextView textTime2 = (TextView)layoutStop.getChildAt(2);
-        TextView text4 = (TextView)layoutLunch.getChildAt(1);
-        TextView text5 = (TextView)layoutAddNote.getChildAt(1);
-
-        text1.setText("Workplace");
-        textDate1.setText("Sun, Mar 27, 2016");
-        textTime1.setText("15:15");
-
-        textDate2.setText("Sun, Mar 27, 2016");
-        textTime2.setText("18:15");
-
-        text4.setText("Add break");
-        text5.setText("Add note");
-
+    @Override
+    public void onClickDateStart() {
 
     }
+
+    @Override
+    public void onClickTimeStart() {
+
+    }
+
+    @Override
+    public void onClickDateEnd() {
+
+    }
+
+    @Override
+    public void onClickTimeEnd() {
+
+    }
+
+    @Override
+    public void onClickBreak() {
+
+    }
+
+    @Override
+    public void onClickAddNote() {
+
+    }
+
+    
 }
--- app/src/main/java/com/lhadalo/oladahl/autowork/AddWorkpassFragment.java
+++ app/src/main/java/com/lhadalo/oladahl/autowork/AddWorkpassFragment.java
@@ -4,33 +4,100 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AddWorkpassFragment extends Fragment {
     private OnFragmentInteraction callback;
+    private EventListener listener = new EventListener();
+    private LinearLayout layoutWorkplace;
+    private LinearLayout layoutStart;
+    private LinearLayout layoutStop;
+    private LinearLayout layoutBreak;
+    private LinearLayout layoutAddNote;
+    private TextView txtWorkplace;
+
+    private TextView txtDateStart;
+    private TextView txtTimeStart;
+
+    private TextView txtDateEnd;
+    private TextView txtTimeEnd;
+
+    private TextView txtBreak;
+    private TextView txtAddNote;
 
     interface OnFragmentInteraction {
-
+        void onClickWorkplace();
+        void onClickDateStart();
+        void onClickTimeStart();
+        void onClickDateEnd();
+        void onClickTimeEnd();
+        void onClickBreak();
+        void onClickAddNote();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //View view = inflater.inflate(R.layout.fragment_add_workpass, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_workpass, container, false);
 
-       // initComponents(view);
-        return null;
+        initComponents(view);
+        return view;
     }
 
     private void initComponents(View view) {
+        Toolbar toolbar = (Toolbar)view.findViewById(R.id.toolbar_1);
+        AppCompatActivity activity = (AppCompatActivity)getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        activity.setTitle("Add workpass");
+
+        layoutWorkplace = (LinearLayout) view.findViewById(R.id.txt_layout_workplace);
+        layoutStart = (LinearLayout) view.findViewById(R.id.txt_layout_start);
+        layoutStop = (LinearLayout) view.findViewById(R.id.txt_layout_stop);
+        layoutBreak = (LinearLayout) view.findViewById(R.id.txt_layout_lunch);
+        layoutAddNote = (LinearLayout) view.findViewById(R.id.txt_layout_add_notes);
+
+        txtWorkplace = (TextView) layoutWorkplace.getChildAt(1);
+        txtDateStart = (TextView) layoutStart.getChildAt(1);
+        txtTimeStart = (TextView) layoutStart.getChildAt(2);
+
+        txtDateEnd = (TextView) layoutStop.getChildAt(1);
+
+        txtTimeEnd = (TextView) layoutStop.getChildAt(2);
+        txtBreak = (TextView) layoutBreak.getChildAt(1);
+        txtAddNote = (TextView) layoutAddNote.getChildAt(1);
+
+        txtWorkplace.setText("Workplace");
+        txtDateStart.setText("Sun, Mar 27, 2016");
+        txtTimeStart.setText("15:15");
+
+        txtDateEnd.setText("Sun, Mar 27, 2016");
+        txtTimeEnd.setText("18:15");
+
+        txtBreak.setText("Add break");
+        txtAddNote.setText("Add note");
 
         initListeners();
     }
 
     private void initListeners() {
+        txtWorkplace.setOnClickListener(listener);
+
+        txtDateStart.setOnClickListener(listener);
+        txtTimeStart.setOnClickListener(listener);
+
+        txtDateEnd.setOnClickListener(listener);
+        txtTimeEnd.setOnClickListener(listener);
+
+        txtBreak.setOnClickListener(listener);
+        txtAddNote.setOnClickListener(listener);
 
     }
 
@@ -43,5 +110,34 @@ public class AddWorkpassFragment extends Fragment {
             Log.e("", context.getClass().getCanonicalName() +
                     " must implement OnFragmentInteraction");
         }
+    }
+
+    private class EventListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            if(view.equals(txtWorkplace)){
+                callback.onClickWorkplace();
+            }
+            else if(view.equals(txtDateStart)){
+                callback.onClickDateStart();
+            }
+            else if(view.equals(txtTimeStart)){
+                callback.onClickTimeStart();
+            }
+            else if(view.equals(txtDateEnd)){
+                callback.onClickDateEnd();
+            }
+            else if(view.equals(txtTimeEnd)){
+                callback.onClickTimeEnd();
+            }
+            else if(view.equals(txtBreak)){
+                callback.onClickBreak();
+            }
+            else if(view.equals(txtAddNote)){
+                callback.onClickAddNote();
+            }
+        }
+
     }
 }
