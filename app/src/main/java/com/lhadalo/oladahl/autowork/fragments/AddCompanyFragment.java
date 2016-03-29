4a5c5163772c9b7dbcfaff415f5c44d6846c042b
Stoffe1985 <christofferwiregren1985@gmail.com>
2016-03-29 10:21:10 +0200

    buggfix

---
+++ app/src/main/java/com/lhadalo/oladahl/autowork/fragments/AddCompanyFragment.java
@@ -0,0 +1,15 @@
+package com.lhadalo.oladahl.autowork.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lhadalo.oladahl.autowork.R;
+
+public class AddCompanyFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_add__company);
+    }
+}
