af6ee7b2d253bea140d49a3670ff4c34c1703949
Henrik <tykesson89@gmail.com>
2016-04-05 17:27:07 +0200

    BufferDatabase

---
+++ app/src/main/java/com/lhadalo/oladahl/autowork/BufferDatabase.java
@@ -0,0 +1,27 @@
+package com.lhadalo.oladahl.autowork;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Henrik on 2016-04-05.
+ */
+public class BufferDatabase extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 2;
+    public static final String DATABASE_NAME = "AutoWork_Buffer_DB";
+
+    public BufferDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
