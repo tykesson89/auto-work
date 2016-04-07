dc94bbc6ccf646c9d5741285c6634242f8def7fc
Stoffe1985 <christofferwiregren1985@gmail.com>
2016-04-07 12:30:28 +0200

    .
    .

---
+++ app/src/main/java/com/lhadalo/oladahl/autowork/activities/AddCompanyActivity.java
@@ -0,0 +1,50 @@
+package com.lhadalo.oladahl.autowork.activities;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.lhadalo.oladahl.autowork.R;
+import com.lhadalo.oladahl.autowork.SQLiteDB;
+import com.lhadalo.oladahl.autowork.Tag;
+import com.lhadalo.oladahl.autowork.fragments.AddCompanyFragment;
+import com.lhadalo.oladahl.autowork.fragments.SettingsFragment;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import UserPackage.User;
+
+public class AddCompanyActivity extends AppCompatActivity implements AddCompanyFragment.OnFragmentInteraction {
+    private AddCompanyFragment fragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_company);
+        initFragment();
+
+    }
+
+    private void initFragment() {
+        fragment = new AddCompanyFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container_add_company, fragment).commit();
+    }
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+
+
+
+}
+
---
+++ app/src/main/java/com/lhadalo/oladahl/autowork/fragments/AddCompanyFragment.java
@@ -0,0 +1,62 @@
+package com.lhadalo.oladahl.autowork.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.lhadalo.oladahl.autowork.R;
+
+public class AddCompanyFragment extends Fragment {
+    private OnFragmentInteraction callback;
+    private Button btnAdd, btnChange, btnDelete;
+    private EditText edCompany, edHourly;
+    private Spinner spinner;
+
+
+
+
+
+    public interface OnFragmentInteraction {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_company, container, false);
+
+        initComponents(view);
+        return view;
+    }
+
+    private void initComponents(View view) {
+
+    }
+
+    private void initListeners() {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            callback = (OnFragmentInteraction) context;
+        } catch (ClassCastException e) {
+            Log.e("", context.getClass().getCanonicalName() +
+                    " must implement OnFragmentInteraction");
+        }
+    }
+
+
+
+}
