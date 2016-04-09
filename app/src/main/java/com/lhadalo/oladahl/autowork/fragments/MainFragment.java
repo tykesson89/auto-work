c8b1571b35e372fce6f1ac2bd7b3b6c9cc13bc97
Ola Dahl <oladahl.lel@gmail.com>
2016-04-09 19:00:07 +0200

    feat: Ändra utseende och lägg till lista till MainActivity, flytta på knappar i appbar

--- app/src/main/java/com/lhadalo/oladahl/autowork/fragments/MainFragment.java
+++ app/src/main/java/com/lhadalo/oladahl/autowork/fragments/MainFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.lhadalo.oladahl.autowork.ListAdapter;
 import com.lhadalo.oladahl.autowork.R;
 
 /**
@@ -22,9 +25,10 @@ import com.lhadalo.oladahl.autowork.R;
  */
 public class MainFragment extends Fragment {
     private OnFragmentInteraction callback;
-    private TextView tvSalary,tvSalaryPass,tvNextPass,tvHours,tvHoursPass;
+    private TextView tvSalary,tvSalaryPass,tvHours,tvHoursPass;
     private Toolbar toolbar;
-    private Button btnTry;
+    private RecyclerView mainRecyclerList;
+    //private Button btnTry;
 
     public interface OnFragmentInteraction{
         void onActionSettingsPressed();
@@ -54,23 +58,29 @@ public class MainFragment extends Fragment {
     private void initComponents(View view){
         tvSalary = (TextView)view.findViewById(R.id.tvSalary);
         tvSalaryPass = (TextView)view.findViewById(R.id.tvSalaryPass);
-        tvNextPass = (TextView)view.findViewById(R.id.tvNextPass);
+        //tvNextPass = (TextView)view.findViewById(R.id.tvNextPass);
         tvHours = (TextView)view.findViewById(R.id.tvHours);
         tvHoursPass = (TextView)view.findViewById(R.id.tvHoursPass);
-        btnTry=(Button)view.findViewById(R.id.button_addCompany_try);
+        //btnTry=(Button)view.findViewById(R.id.button_addCompany_try);
 
+        mainRecyclerList = (RecyclerView)view.findViewById(R.id.main_recycler_list);
+        mainRecyclerList.setHasFixedSize(true);
+
+        AppCompatActivity context = (AppCompatActivity)getActivity();
+        RecyclerView.LayoutManager mLinearLayoutManager = new LinearLayoutManager(context);
+        mainRecyclerList.setLayoutManager(mLinearLayoutManager);
 
         initListeners();
     }
 
     private void initListeners(){
 
-        btnTry.setOnClickListener(new View.OnClickListener() {
+        /*btnTry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 callback.onButtonClickTry();
             }
-        });
+        });*/
 
     }
 
@@ -78,6 +88,10 @@ public class MainFragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         this.callback = (OnFragmentInteraction)context;
+    }
+
+    public void setListAdapter(ListAdapter adapter){
+        mainRecyclerList.setAdapter(adapter);
     }
 
     public void setTextTvSalary(String str){
@@ -90,7 +104,7 @@ public class MainFragment extends Fragment {
     }
     public void setTextTvNextPass(String str){
 
-        tvNextPass.setText(str);
+        //tvNextPass.setText(str);
     }
     public void setTextTvHours(String str){
 
